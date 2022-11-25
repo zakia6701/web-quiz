d973ce753c88bf82d326c6a2e36a232669965551
zaki ahmed <zakiahmed@zakis-MacBook-Pro.local>
2022-11-25 00:12:23 -0600

    first commit

---
+++ questions.js
@@ -0,0 +1,74 @@
+// Question bank, option = Multiple Choice Questions and Answers
+
+var questions= [
+ 
+    // QUESTION 1  
+    {
+      title: "Which HTML element is used to embed executable code or data and is typically used to embed or refer to JavaScript code?",
+      option: ["<script>", "<javascript>", "<js>", "All the above"],
+      answer: "<script>"
+    },
+    
+    // QUESTION 2  
+    {
+      title: " What is the DOM.",
+      option: ["Document Object Model", "Document Orientation Marker", "Do Not Open","Dark Over Matte"],
+      answer: "Document Object Model"
+    },
+    
+    // QUESTION 3  
+    {
+      title: " Correct place to insert a JavaScript in HTML",
+      option: ["Head", "Body", "Both", "All of the above"],
+      answer: "Both"
+    },
+    
+    // QUESTION 4  
+     {
+      title: " _____ is one of the most-used APIs on the Web because it allows code running in a browser to access and interact with every node in the document.",
+      option: ["AWS", "DOM", "Shopify", "Twitter"],
+      answer: "DOM"
+    },
+    
+    // QUESTION 5  
+    {
+      title:
+        "A _______ is a sequence of characters used to represent text.:",
+        option: ["variable", "string", "HTML", "for loop", ],
+      answer: "string"
+    },
+    
+    // QUESTION 6  
+    {
+      title:
+        "An _______ statement executes a statement if a specified condition is true. If the condition is false, another statement in the optional ____ clause will be executed.",
+        option: ["True/False", "What if", "if/else", "yes/no"],
+      answer: "if/else"
+    },
+    
+    // QUESTION 7  
+    {
+      title: "what is an else if clause?",
+      option: ["Multiple if...else statements nested", "Multiple characters strung together", "and array", "All of the above",],
+      answer: "Multiple if...else statements nested"
+    },
+    
+    // QUESTION 8  
+    {
+      title: "The _____ object enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.",
+      option: ["Alert", "Confirm", "Array", "All the above"],
+      answer: "Array"
+    },
+    // QUESTION 9  
+    {
+      title: "Which of the following is an Object constructor's behavior?.",
+      option: ["If the value is null or undefined, it will create and return an empty object.", "If the value is an object already, it will return the value.", "Otherwise, it will return an object of a Type that corresponds to the given value.", "All of the above"],
+      answer: "All of the above"
+    },
+    // QUESTION 10  
+    {
+      title: "A function definition (also called a function declaration, or function statement) consists of the function keyword, followed by:",
+      option: ["The name of the function.", "A list of parameters to the function, enclosed in parentheses and separated by commas.", "The JavaScript statements that define the function, enclosed in curly brackets, { /* … */ }.", "All the above"],
+      answer: "All of the above"
+    }
+    ]
---
+++ script.js
@@ -0,0 +1,169 @@
+// DOM elements
+let timerEl = document.querySelector("#time");
+let choiceEl = document.querySelector("#option");
+let subBtn = document.querySelector("#submit");
+let repyEl = document.querySelector("#reply");
+let startBtn = document.querySelector("#begin");
+let questionsEl = document.querySelector("#questions");
+let initialsEl = document.getElementById("initials");
+
+// functions
+let currentQuestionsIndex = 0;
+let time = questions.length * 30;
+let timerId;
+
+function beginQuiz() {
+  // hidden at begin screen
+  let beginScreenEl = document.getElementById("main-page");
+  beginScreenEl.setAttribute("class", "hide");
+
+  // show questions section
+  questionsEl.removeAttribute("class");
+
+  // begin timer
+  timerId = setInterval(clockTick, 1000);
+
+  // show begining time
+  timerEl.textContent = time;
+
+  getQuestions();
+}
+
+function getQuestions() {
+  // questions from array
+  console.log("questions", questions);
+  let currentQuestions = questions[currentQuestionsIndex];
+
+  // New question
+  let titleEl = document.getElementById("questions-title");
+  titleEl.textContent = currentQuestions.title;
+
+  // replace old question with new
+  choiceEl.innerHTML = "";
+
+  // loop over Multiple Question Choice
+  currentQuestions.option.forEach(function (option, i) {
+    // create new button for each choice selection
+    let choiceNode = document.createElement("button");
+    choiceNode.setAttribute("class", "option");
+    choiceNode.setAttribute("value", option);
+
+    choiceNode.textContent = i + 1 + ". " + option;
+
+    // attach click event listener to each Anwser
+    choiceNode.onclick = questionsClick;
+
+    // display on the page
+    optionElement.appendChild(choiceNode);
+  });
+}
+
+function questionsClick() {
+  // check if user guessed wrong
+  if (this.value !== questions[currentQuestionsIndex].answer) {
+    // penalize time
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+    // Display updated time on page
+    timerEl.textContent = time;
+    replyEl.textContent = "Wrong!";
+    replyEl.style.color = "red";
+    replyEl.style.fontSize = "400%";
+  } else {
+    replyEl.textContent = "Correct!";
+    replyEl.style.color = "green";
+    replyEl.style.fontSize = "400%";
+  }
+
+  // Display Correct or Incorrect Reply
+  replyEl.setAttribute("class", "reply");
+  setTimeout(function () {
+    replyEl.setAttribute("class", "reply hide");
+  }, 500);
+
+  // Next Multiple Choice Question
+  currentQuestionsIndex++;
+
+  // Time check
+  if (currentQuestionsIndex === questions.length) {
+    endQuiz();
+  } else {
+    getQuestions();
+  }
+}
+
+function endQuiz() {
+  // End timer
+  clearInterval(timerId);
+
+  // display last end page
+  let endPageElement = document.getElementById("end-page");
+  endPageElement.removeAttribute("class");
+
+  // show final score
+  let finalScoreElement = document.getElementById("final-score");
+  finalScoreElement.textContent = time;
+
+  // hide questions section
+  questionsEl.setAttribute("class", "hide");
+}
+
+function clockTick() {
+  // update time
+  time--;
+  timerElement.textContent = time;
+
+  // check if user ran out of time
+  if (time <= 0) {
+    endQuiz();
+  }
+}
+
+function saveTopscore() {
+  // Record value input
+  let initials = initialsEl.value.trim();
+
+  console.log('initials', initials);
+
+  if (initials !== "") {
+    console.log('initials', initials);
+    // Saved scores from localstorage, or if not any, set to empty array
+    let topscores = JSON.parse(window.localStorage.getItem("topscores")) || [];
+    console.log('topscores 1', topscores);
+    // Create new score object for current user
+    let newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    // save to localstorage
+    topscores.push(newScore);
+    console.log('topscores 2', topscores);
+    window.localStorage.setItem("topscores", JSON.stringify(topscores));
+
+    // Move to next display page
+    window.location.href = "topscore.html";
+  }
+}
+
+function checkForEnter(event) {
+  // represents the enter key
+  if (event.key === "Enter") {
+    saveTopscore();
+  }
+}
+
+// submit initials
+submBtn.addEventListener("click", saveTopscore);
+
+// begin quiz
+
+
+startBtn.addEventListener("click", beginQuiz);
+
+initialsEl.onkeyup = checkForEnter;
+
+// Scores JS functions
